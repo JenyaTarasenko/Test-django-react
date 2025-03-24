bda1189553998b3d7ef2d03ca33dcf80fee316de
Kiril <kiril@Mac-jenya.local>
2025-03-24 14:04:25 +0200

    react-project-front

---
+++ frontend/my-react-app/src/components/comment/comment.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+import React from 'react';
+
+
+
+function Comment() {
+    return (
+        <div className='hello'>
+             <h1>Hello world</h1>
+        </div>
+       
+    )
+}
+
+export default Comment;
